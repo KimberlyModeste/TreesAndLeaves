0e5495128b8192318112271d8b1334fe2a27de24
Kimberly Modeste <KimberlyJModeste@gmail.com>
2021-11-19 14:15:21 -0500

    Reset to basic

--- Hw3/main.cpp
+++ Hw3/main.cpp
@@ -698,11 +698,11 @@ void main(int argc, char** argv)
     glutInitWindowSize(600, 600);     // set window size
     glutCreateWindow("Trees and Leaves"); // open the screen window and set the name
    //1-4 = Basic 5-8 CreativeB
-    gid = 8;
+    gid = 1;
 
 
     //Basic Drawing A
-    glutDisplayFunc(basicADisplay); 
+     glutDisplayFunc(basicADisplay); 
 
     //Basic Drawing B
     //glutDisplayFunc(basicBDisplay);    
@@ -712,7 +712,7 @@ void main(int argc, char** argv)
 
     //Creative Drawing B
     //IT TAKES 1 MINUTE AND 16 SECONDS FOR THIS TO FINISH RUNNING
-     glutDisplayFunc(creativeBDisplay);
+    //glutDisplayFunc(creativeBDisplay);
 
     glutMainLoop();
 }
